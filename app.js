a869583effa5900e5f1aa644422e21dd864422d5
Hasan Tayyar <tayyar.besik@gmail.com>
2019-10-11 12:21:09 +0200

    joy

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 8080
 
-const randomTld = () => (['com', 'net', 'org', 'io', 'cc'])[Math.floor(Math.random() * Math.floor(4))];
-const randomStr = () =>  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+const randomTld = () => (['com', 'net', 'org', 'io', 'cc'])[Math.floor(Math.random() * Math.floor(4))]
+const randomStr = () =>  Math.random().toString(36).substring(2, 15)
 const newEmail = () => `${randomStr()}@${randomStr()}.${randomTld()}`
 
 
-app.get('/', (req, res) => res.send(newEmail()))
+app.get('/*', (req, res) => res.send(`
+<a href='/emails/${randomStr()}'/>next</a>
+<br/>
+${newEmail()}
+`
+))
 
-app.listen(process.env.PORT, () => console.log(`app listening on port ${process.env.PORT}!`))
+app.listen(port, () => console.log(`app listening on port ${port}!`))
 
