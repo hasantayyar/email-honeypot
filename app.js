b96564090c4b12c9435e527944ff5aec17c62494
Hasan Tayyar <tayyar.besik@gmail.com>
2019-10-11 12:31:32 +0200

    lint

--- app.js
+++ app.js
@@ -2,17 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
-const randomTld = () => (['com', 'net', 'org', 'io', 'cc'])[Math.floor(Math.random() * Math.floor(4))]
-const randomStr = () =>  Math.random().toString(36).substring(2, 15)
-const newEmail = () => `${randomStr()}@${randomStr()}.${randomTld()}`
+const tld = () => (['com', 'net', 'org', 'io', 'cc'])[Math.floor(Math.random() * Math.floor(4))]
+const str = () =>  Math.random().toString(36).substring(2, 15)
+const email = () => `${str()}@${str()}.${tld()}`
 
-
-app.get('/*', (req, res) => res.send(`
-<a href='/emails/${randomStr()}'/>next</a>
-<br/>
-${newEmail()}
-`
-))
-
+app.get('/*', (req, res) => res.send(`<a href='/emails/${randomStr()}'/>next</a><br/>${newEmail()}`))
 app.listen(port, () => console.log(`app listening on port ${port}!`))
 
