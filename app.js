189a58ba0ee9ec043ee68ec32d22bd2a28c3a38d
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2019-11-10 23:49:19 +0100

    minor

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
-
-const tld = () => (['com', 'net', 'org', 'io', 'cc'])[Math.floor(Math.random() * Math.floor(4))]
+const tld = () => (['com', 'net', 'org', 'io', 'co.uk'])[Math.floor(Math.random() * Math.floor(4))]
 const str = () =>  Math.random().toString(36).substring(2, 15)
 const email = () => `${str()}@${str()}.${tld()}`
 
-app.get('/*', (req, res) => res.send(`<a href='/emails/${randomStr()}'/>next</a><br/>${newEmail()}`))
-app.listen(port, () => console.log(`app listening on port ${port}!`))
-
+app.get('/*', (req, res) => res.send(`<a href='/emails/${str()}'/>next</a><br/>${email()}`))
+app.listen(port, () => console.log(`listening on port ${port}!`))
